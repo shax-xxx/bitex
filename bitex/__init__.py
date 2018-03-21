dca6fce35d103be7cf8194f5d65f1bb7fe14dd5b
ajeep8 <ajeep8@gmail.com>
2018-03-21 19:16:26 +0800

    add Version Info

--- bitex/__init__.py
+++ bitex/__init__.py
@@ -6,4 +6,5 @@ from bitex.interfaces import Kraken, Bitfinex, Bitstamp, CCEX, Coincheck
 from bitex.interfaces import Cryptopia, Gemini, ItBit, OKCoin, RockTradingLtd
 from bitex.interfaces import Yunbi, Bittrex, Poloniex, Quoine, QuadrigaCX
 from bitex.interfaces import Vaultoro, HitBtc, Bter, GDAX
-
+from bitex._version import __version__
+version=__version__
